d17d2ab547ced5e9083d468cf55abfab19b5e1b1
JESUS ALEJANDRO PEINADO AVILA <2020030720@upsin.edu.mx>
2022-10-27 18:26:37 -0600

    Codificacion del botón calcular

--- js/examen.js
+++ js/examen.js
@@ -21,4 +21,98 @@ function Mover(Origen){
     }
     }
     
+    function Calcular(){
+        let cantidad= document.getElementById('cantidad').value;
+        let MD= document.getElementById('opDestino');
+        let Origen = document.getElementById('opOrigen');
+        let subtotal=0;
+        let comision=0;
+        let total=0;
+    
+        if(Origen.value=="1"){
+            switch(MD.value){
+                case "1":
+                    subtotal=(cantidad/19.85).toFixed(2);
+                    comision=(subtotal*0.03).toFixed(2);
+                    total=(parseFloat(subtotal) + parseFloat(comision)).toFixed(2);
+                    break;
+                case "2":
+                    subtotal=((cantidad/19.85)*1.35).toFixed(2);
+                    comision=(subtotal*0.03).toFixed(2);
+                    total=(parseFloat(subtotal) + parseFloat(comision)).toFixed(2);
+                    break;
+                case "3":
+                subtotal=((cantidad/19.85)*0.99).toFixed(2);
+                comision=(subtotal*0.03).toFixed(2);
+                total=(parseFloat(subtotal) + parseFloat(comision)).toFixed(2);
+                    break;    
+                default:
+                    break;    
+            }
+        }else if(Origen.value=="2"){
+            switch(MD.value){
+                case "1":
+                    subtotal=(cantidad*19.85).toFixed(2);
+                    comision=(subtotal*0.03).toFixed(2);
+                    total=(parseFloat(subtotal) + parseFloat(comision)).toFixed(2);
+                    break;
+                case "2":
+                    subtotal=(cantidad*1.35).toFixed(2);
+                    comision=(subtotal*0.03).toFixed(2);
+                    total=(parseFloat(subtotal) + parseFloat(comision)).toFixed(2);
+                    break;
+                case "3":
+                subtotal=(cantidad*0.99).toFixed(2);
+                comision=(subtotal*0.03).toFixed(2);
+                total=(parseFloat(subtotal) + parseFloat(comision)).toFixed(2);
+                    break;    
+                default:
+                    break;    
+            }
+        }else if(Origen.value=="3"){
+            switch(MD.value){
+                case "1":
+                    subtotal=((cantidad*19.85)/1.35).toFixed(2);
+                    comision=(subtotal*0.03).toFixed(2);
+                    total=(parseFloat(subtotal) + parseFloat(comision)).toFixed(2);
+                    break;
+                case "2":
+                    subtotal=((cantidad/1.35)).toFixed(2);
+                    comision=(subtotal*0.03).toFixed(2);
+                    total=(parseFloat(subtotal) + parseFloat(comision)).toFixed(2);
+                    break;
+                case "3":
+                subtotal=((cantidad*0.99)/1.35).toFixed(2);
+                comision=(subtotal*0.03).toFixed(2);
+                total=(parseFloat(subtotal) + parseFloat(comision)).toFixed(2);
+                    break;    
+                default:
+                    break;    
+            }
+        }else if(Origen.value=="4"){
+            switch(MD.value){
+                case "1":
+                    subtotal=((cantidad*19.85)/0.99).toFixed(2);
+                    comision=(subtotal*0.03).toFixed(2);
+                    total=(parseFloat(subtotal) + parseFloat(comision)).toFixed(2);
+                    break;
+                case "2":
+                    subtotal=(cantidad/0.99).toFixed(2);
+                    comision=(subtotal*0.03).toFixed(2);
+                    total=(parseFloat(subtotal) + parseFloat(comision)).toFixed(2);
+                    break;
+                case "3":
+                subtotal=((cantidad*1.95)/0.99).toFixed(2);
+                comision=(subtotal*0.03).toFixed(2);
+                total=(parseFloat(subtotal) + parseFloat(comision)).toFixed(2);
+                    break;    
+                default:
+                    break;    
+            }
+        }
+        document.getElementById('subTotal').value=subtotal;
+        document.getElementById('totalComision').value=comision;
+        document.getElementById('totalPagar').value=total;
+    
+    }
    
